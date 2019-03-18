e0627f0bb52e8b31bdd2e2f730e16253ffb72219
Wiktor Kuśmirek <kusmirekwiktor@gmail.com>
2019-03-18 15:05:45 +0100

    scripts for exomecopy caller

---
+++ scripts/exomecopy/knn.sh
@@ -0,0 +1,19 @@
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_50.csv', '/tmp/exomecopy_results_knn_50')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_100.csv', '/tmp/exomecopy_results_knn_100')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_150.csv', '/tmp/exomecopy_results_knn_150')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_200.csv', '/tmp/exomecopy_results_knn_200')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_250.csv', '/tmp/exomecopy_results_knn_250')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_300.csv', '/tmp/exomecopy_results_knn_300')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_350.csv', '/tmp/exomecopy_results_knn_350')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_400.csv', '/tmp/exomecopy_results_knn_400')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_450.csv', '/tmp/exomecopy_results_knn_450')"
+
+docker run --rm -v /tmp:/tmp -w /tmp biodatageeks/cnv-opt-exomecopycov Rscript -e "library('EXOMECOPYCOV');run_EXOMECOPYCOV('/tmp/qc_cov_table.csv', '/tmp/qc_bed.bed', '/tmp/reference_sample_set_knn_500.csv', '/tmp/exomecopy_results_knn_500')"
